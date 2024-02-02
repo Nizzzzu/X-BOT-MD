156decb8aa88fb2160b4f11a6abe889235761897
Nizzzzu <157376768+Nizzzzu@users.noreply.github.com>
2024-02-02 15:28:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let mm = process.env.MENTION_AUDIO;
 global.mentionaudio = mm.split(",");
 global.owner = oo.split(",");
 module.exports = {
-  mongodb: process.env.MONGODB_URL || "",
+  mongodb: process.env.MONGODB_URL || "mongodb+srv://Mongo:lionorwot @cluster0.axxm9xq.mongodb.net//",
   BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;Aswin Sparky;https://graph.org/file/9b477aaa6b90bbb46e4be.mp4",
   STICKER_DATA: process.env.STICKER_DATA || "X-BOTMD;ASWIN SPARKY",
   SESSION_ID: process.env.SESSION_ID || "X-BOT-MD:WeirdoPlanted
